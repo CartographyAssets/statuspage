8fba6098d151638886eee1a5a96135ce370b3bdf
Nexoness <44317530+NexonessGithub@users.noreply.github.com>
2025-04-06 12:55:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const maxDays = 30;
 let cloneId = 0;
 let tooltipTimeout = null;
 let maintenanceData = {};
-let maintenanceLoaded = false;
 
 async function genReportLog(container, key, url) {
   const response = await fetch("logs/" + key + "_report.log");
@@ -17,9 +16,8 @@ async function genReportLog(container, key, url) {
   const statusStream = constructStatusStream(key, url, normalized);
   container.appendChild(statusStream);
 
-  if (key === "cartographyassets" && !maintenanceLoaded) {
+  if (key === "cartographyassets") {
     await loadMaintenanceData();
-    maintenanceLoaded = true;
   }
 }
 
@@ -39,73 +37,26 @@ async function loadMaintenanceData() {
   });
 }
 
-function normalizeData(statusLines) {
-  const rows = statusLines.split("\n");
-  const dateNormalized = splitRowsByDate(rows);
-
-  let relativeDateMap = {};
-  const now = Date.now();
-  for (const [key, val] of Object.entries(dateNormalized)) {
-    if (key == "upTime") {
-      continue;
-    }
-
-    const relDays = getRelativeDays(now, new Date(key).getTime());
-    relativeDateMap[relDays] = getDayAverage(val);
+function constructStatusStream(key, url, uptimeData) {
+  let streamContainer = templatize("statusStreamContainerTemplate");
+  for (var ii = maxDays - 1; ii >= 0; ii--) {
+    let line = constructStatusLine(key, ii, uptimeData[ii]);
+    streamContainer.appendChild(line);
   }
 
-  relativeDateMap.upTime = dateNormalized.upTime;
-  return relativeDateMap;
-}
+  const lastSet = uptimeData[0];
+  const color = getColor(lastSet);
 
-function splitRowsByDate(rows) {
-  let dateValues = {};
-  let sum = 0,
-    count = 0;
-  for (var ii = 0; ii < rows.length; ii++) {
-    const row = rows[ii];
-    if (!row) {
-      continue;
-    }
+  const container = templatize("statusContainerTemplate", {
+    title: key,
+    url: url,
+    color: color,
+    status: getStatusText(color),
+    upTime: uptimeData.upTime,
+  });
 
-    const [dateTimeStr, resultStr] = row.split(",", 2);
-    const dateTime = new Date(Date.parse(dateTimeStr.replace(/-/g, "/") + " GMT"));
-    const dateStr = dateTime.toDateString();
-
-    let resultArray = dateValues[dateStr];
-    if (!resultArray) {
-      resultArray = [];
-      dateValues[dateStr] = resultArray;
-      if (Object.keys(dateValues).length > maxDays) {
-        break;
-      }
-    }
-
-    let result = 0;
-    if (resultStr.trim() == "success") {
-      result = 1;
-    }
-    sum += result;
-    count++;
-
-    resultArray.push(result);
-  }
-
-  const upTime = count ? ((sum / count) * 100).toFixed(2) + "%" : "--%";
-  dateValues.upTime = upTime;
-  return dateValues;
-}
-
-function getRelativeDays(date1, date2) {
-  return Math.floor(Math.abs((date1 - date2) / (24 * 3600 * 1000)));
-}
-
-function getDayAverage(val) {
-  if (!val || val.length == 0) {
-    return null;
-  } else {
-    return val.reduce((a, v) => a + v) / val.length;
-  }
+  container.appendChild(streamContainer);
+  return container;
 }
 
 function constructStatusLine(key, relDay, upTimeArray) {
@@ -113,6 +64,16 @@ function constructStatusLine(key, relDay, upTimeArray) {
   date.setDate(date.getDate() - relDay);
 
   return constructStatusSquare(key, date, upTimeArray);
+}
+
+function getColor(uptimeVal) {
+  return uptimeVal == null
+    ? "nodata"
+    : uptimeVal == 1
+    ? "success"
+    : uptimeVal < 0.3
+    ? "failure"
+    : "partial";
 }
 
 function constructStatusSquare(key, date, uptimeVal) {
@@ -136,14 +97,66 @@ function constructStatusSquare(key, date, uptimeVal) {
   return square;
 }
 
-function getColor(uptimeVal) {
-  return uptimeVal == null
-    ? "nodata"
-    : uptimeVal == 1
-    ? "success"
-    : uptimeVal < 0.3
-    ? "failure"
-    : "partial";
+function templatize(templateId, parameters) {
+  let clone = document.getElementById(templateId).cloneNode(true);
+  clone.id = "template_clone_" + cloneId++;
+  if (!parameters) {
+    return clone;
+  }
+
+  applyTemplateSubstitutions(clone, parameters);
+  return clone;
+}
+
+function applyTemplateSubstitutions(node, parameters) {
+  const attributes = node.getAttributeNames();
+  for (var ii = 0; ii < attributes.length; ii++) {
+    const attr = attributes[ii];
+    const attrVal = node.getAttribute(attr);
+    node.setAttribute(attr, templatizeString(attrVal, parameters));
+  }
+
+  if (node.childElementCount == 0) {
+    node.innerText = templatizeString(node.innerText, parameters);
+  } else {
+    const children = Array.from(node.children);
+    children.forEach((n) => {
+      applyTemplateSubstitutions(n, parameters);
+    });
+  }
+}
+
+function templatizeString(text, parameters) {
+  if (parameters) {
+    for (const [key, val] of Object.entries(parameters)) {
+      text = text.replaceAll("$" + key, val);
+    }
+  }
+  return text;
+}
+
+function getStatusText(color) {
+  return color == "nodata"
+    ? "No Data Available"
+    : color == "success"
+    ? "Fully Operational"
+    : color == "failure"
+    ? "Major Outage"
+    : color == "partial"
+    ? "Partial Outage"
+    : "Unknown";
+}
+
+function getStatusDescriptiveText(color) {
+  return color == "nodata"
+    ? "No Data Available: Health check was not performed."
+    : color == "success"
+    ? "No downtime recorded on this day."
+    : color == "failure"
+    ? "Major outages recorded on this day."
+    : color == "partial"
+    ? "Partial outages recorded on this day."
+    : "Unknown";
 }
 
 function getTooltip(key, date, color, maintenanceInfo = []) {
@@ -188,68 +201,6 @@ function hideTooltip() {
     const toolTipDiv = document.getElementById("tooltip");
     toolTipDiv.style.opacity = "0";
   }, 1000);
-}
-
-function getStatusText(color) {
-  return color == "nodata"
-    ? "No Data Available"
-    : color == "success"
-    ? "Fully Operational"
-    : color == "failure"
-    ? "Major Outage"
-    : color == "partial"
-    ? "Partial Outage"
-    : "Unknown";
-}
-
-function getStatusDescriptiveText(color) {
-  return color == "nodata"
-    ? "No Data Available: Health check was not performed."
-    : color == "success"
-    ? "No downtime recorded on this day."
-    : color == "failure"
-    ? "Major outages recorded on this day."
-    : color == "partial"
-    ? "Partial outages recorded on this day."
-    : "Unknown";
-}
-
-function templatize(templateId, parameters) {
-  let clone = document.getElementById(templateId);
-  if (!clone) {
-    console.warn("Template not found:", templateId);
-    return document.createElement("div");
-  }
-  clone = clone.cloneNode(true);
-  clone.id = "template_clone_" + cloneId++;
-  if (!parameters) return clone;
-  applyTemplateSubstitutions(clone, parameters);
-  return clone;
-}
-
-function applyTemplateSubstitutions(node, parameters) {
-  const attributes = node.getAttributeNames();
-  for (var ii = 0; ii < attributes.length; ii++) {
-    const attr = attributes[ii];
-    const attrVal = node.getAttribute(attr);
-    node.setAttribute(attr, templatizeString(attrVal, parameters));
-  }
-
-  if (node.childElementCount == 0) {
-    node.innerText = templatizeString(node.innerText, parameters);
-  } else {
-    const children = Array.from(node.children);
-    children.forEach((n) => {
-      applyTemplateSubstitutions(n, parameters);
-    });
-  }
-}
-
-function templatizeString(text, parameters) {
-  for (const [key, val] of Object.entries(parameters)) {
-    text = text.replaceAll("$" + key, val);
-  }
-  return text;
 }
 
 async function genAllReports() {
